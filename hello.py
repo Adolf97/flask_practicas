33caacd528667124c7b8e79d428e8f5fb70cd584
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-06-11 04:45:37 -0600

    Agregué un índice de navegación al layout. Agregué la plantilla para la ruta de registro

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 from markupsafe import escape
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='dev'
+)
 
 
 @app.add_template_filter
@@ -18,8 +23,10 @@ def repeat(s, n):
 
 
 @app.route('/')
-@app.route('/index')
 def mainPage():
+    print(url_for('mainPage'))
+    print(url_for('helloPage'))
+    print(url_for('code', code='print("Hola")'))
     name = 'adolfo'
     friends = ['Adolfo', 'Carlos', 'Cristian', 'Jorge']
     date = datetime.now()
@@ -49,3 +56,27 @@ def code(code):
     return f'<code>{escape(code)}</code>'
     # Se usa el escape, para ejecutar código como texto plano. Con esto evitamos ataques externos
     #  de códigos maliciosos
+
+
+class RegisterForm(FlaskForm):
+    username = StringField("Nombre de usuario: ")
+    password = PasswordField("Contraseña: ")
+    submit = SubmitField("Registrar")
+
+
+@app.route('/auth/registro', methods=['GET', 'POST'])
+def registro():
+    form = RegisterForm()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if len(username) >= 4 and len(username) <= 25 and len(password) >= 8 and len(password) <= 25:
+            return f'Nombre de usuario: {username} <br >Contraseña: {password}'
+        else:
+            error = """
+            El nombre de usuaio debe ser mayor a 4 caracteres y menor a 25 caracteres.
+            La contraseña debe ser mayor a 8 caracteres y menor a 25 caracteres.
+            """
+            return render_template('auth/register.html', form=form, error=error)
+    return render_template('auth/register.html', form=form)
